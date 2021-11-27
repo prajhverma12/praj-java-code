604f8377dd59ec91de5c2e0c77e81c05d6b8cdda
prajwal <prajh.kool@gmail.com>
2021-11-27 06:30:24 +0530

    FlatMap and MaptoInt Examples

--- JavaStreams/src/FlatMapsAndOptional.java
+++ JavaStreams/src/FlatMapsAndOptional.java
@@ -7,15 +7,37 @@ public class FlatMapsAndOptional {
     public static void main(String[] args) {
 
         List<User> users = Arrays.asList(
-            new User("Peter", 20, Arrays.asList("1", "2")), 
-            new User("Sam", 30, Arrays.asList("3", "5")));
+        new User("Peter", 20, Arrays.asList("1", "2")), 
+        new User("Sam", 30, Arrays.asList("3", "5")));
 
-        //Functional Style to get User with phoneno with 5
+        //Imperative Style to find user with Phone Number 5
+        System.out.println("Imperative Style to find user with Phone Number 5");
+        for(User user : users) {
+            for(String phno : user.getPhoneNumbers()) {
+                if(phno.equals("5")) {
+                    System.out.println("User - " + user.getName() + " is having Phone Number - " + phno);
+                }
+            }
+        }
+
+        //Functional Style using Lambda Expression to find user with Phone Number 5
+        System.out.println("Functional Style using Lambda Expression to find user with Phone Number 5");
         Optional<String> stringOptional = users.stream()
         .map(user -> user.getPhoneNumbers().stream())
         .flatMap(stringStream -> stringStream.filter(phoneNo -> phoneNo.equals("5")))
         .findAny();
-
         stringOptional.ifPresent(phoneNo -> System.out.println(phoneNo));
+
+        // Optional<String> stringOptional1 = users.stream()
+        // .map(new Function<User, Object>() {
+        //     @Override
+        //     public Object apply(User t) {
+        //         return t.getPhoneNumbers().stream();
+        //     };
+        // }).flatMap(new Function<Stream<String>,Stream<? extends Object>>() {
+        //     public java.util.stream.Stream<? extends Object> apply(java.util.stream.Stream<String> t) {
+        //         return t.filter(phoneNo -> phoneNo.equals("5"));
+        //     };
+        // }).findAny();
     }
 }
--- JavaStreams/src/MapToIntAndSum.java
+++ JavaStreams/src/MapToIntAndSum.java
@@ -1,3 +1,41 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.ToIntFunction;
+
 public class MapToIntAndSum {
     
+    public static void main(String[] args) {
+
+        List<User> users = Arrays.asList(
+        new User("Peter", 20, Arrays.asList("1", "2")), 
+        new User("Sam", 30, Arrays.asList("3", "5")));
+
+        //Imperative Style
+        System.out.println("Imperative Style");
+        int sum = 0;
+        for(User user : users) {
+            sum = sum + user.getAge();
+        }
+        System.out.println("The total age is :- " + sum);
+
+        //Functional Style using Anonymous Inner Class
+        System.out.println("Functional Style using Anonymous Inner Class");
+        int sum1 = users.stream().mapToInt(new ToIntFunction<User>() {
+            @Override
+            public int applyAsInt(User value) {
+                return value.getAge();
+            }
+        }).sum();
+        System.out.println("The total age is :- " + sum1);
+
+        //Functional Style using Lambda Expression
+        System.out.println("Functional Style using Lambda Expression");
+        int sum2 = users.stream().mapToInt(user -> user.getAge()).sum();
+        System.out.println("The total age is :- " + sum2);
+
+        //Functional Style using Method Reference
+        System.out.println("Functional Style using Method Reference");
+        int sum3 = users.stream().mapToInt(User :: getAge).sum();
+        System.out.println("The total age is :- " + sum3);
+    }
 }
